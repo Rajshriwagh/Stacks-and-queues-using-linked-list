68d1340c8158fe55c1bdcd88be1a6a55460c8a5b
Rajshriwagh <mahajan.rajshri16@gmail.com>
2022-10-02 21:40:46 +0530

    ADD: Added logic for dequeue operation

--- src/main/java/com/stacksandqueues/App.java
+++ src/main/java/com/stacksandqueues/App.java
@@ -6,22 +6,24 @@ import org.apache.logging.log4j.Logger;
 public class App {
 	private static final Logger logger = LogManager.getLogger(App.class);
 
-	public static void main(String[] args) {
-		logger.info("Implementation of Stack & Queue using Linked List!");
-		Stack s = new Stack();
-		s.push(70);
-		s.push(30);
-		s.push(56);
-		s.showStack();
-		while (!s.isEmpty()) {
-			// s.peek();
-			s.pop();
-		}
-		Queue q = new Queue();
-		q.enqueue(56);
-		q.enqueue(30);
-		q.enqueue(70);
-		logger.info("Queue implementation");
-
-	}
+    public static void main(String[] args) {
+        logger.info("Implementation of Stack & Queue using Linked List!");
+        Stack s = new Stack();
+        s.push(70);
+        s.push(30);
+        s.push(56);
+        s.showStack();
+        while (!s.isEmpty()) {
+            // s.peek();
+            s.pop();
+        }
+        Queue q = new Queue();
+        q.enqueue(56);
+        q.enqueue(30);
+        q.enqueue(70);
+        logger.info("Queue implementation");
+        while (!q.isEmpty()) {
+            q.dequeue();
+        }
+    }
 }
